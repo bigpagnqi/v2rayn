b9ec369b4eb7c00268b63c49a295090835bda988
2dust <qingyede@gmail.com>
2019-08-27 09:02:20 +0800

    up speed

---
+++ v2rayN/v2rayN/Mode/ServerStatistics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace v2rayN.Mode
+{
+    class ServerStatistics
+    {
+        public string name;
+        public string address;
+        public int port;
+        public string path;
+        public ulong totalUp;
+        public ulong totalDown;
+        public ulong todayUp;
+        public ulong todayDown;
+
+        public ServerStatistics() { }
+        public ServerStatistics(string name, string addr, int port, string path, ulong totalUp, ulong totalDown, ulong todayUp, ulong todayDown)
+        {
+            this.name = name;
+            this.address = addr;
+            this.port = port;
+            this.path = path;
+            this.totalUp = totalUp;
+            this.totalDown = totalDown;
+            this.todayUp = todayUp;
+            this.todayDown = todayDown;
+        }
+    }
+}
