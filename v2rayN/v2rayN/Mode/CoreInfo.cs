08d5869efa145dda92b53b43afb1b99362cb399d
2dust <31833384+2dust@users.noreply.github.com>
2022-04-25 20:27:22 +0800

    Update AssemblyInfo.cs

---
+++ v2rayN/v2rayN/Mode/CoreInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace v2rayN.Mode
+{
+    [Serializable]
+    public class CoreInfo
+    {
+        public ECoreType coreType { get; set; }
+
+        public List<string> coreExes { get; set; }
+
+        public string arguments { get; set; }
+
+        public string coreUrl { get; set; }
+
+        public string match { get; set; }
+    }
+}
