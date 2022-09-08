ebf635b94953e52951b73a05f8cc37d00bfed0f5
2dust <31833384+2dust@users.noreply.github.com>
2022-09-08 20:52:51 +0800

    Update v2rayN.csproj

---
+++ v2rayN/v2rayN/Mode/ECoreType.cs
@@ -0,0 +1,17 @@
+﻿
+namespace v2rayN.Mode
+{
+    public enum ECoreType
+    {
+        v2fly = 1,
+        Xray = 2,
+        SagerNet = 3,
+        v2fly_v5 = 4,
+        clash = 11,
+        clash_meta = 12,
+        hysteria = 21,
+        naiveproxy = 22,
+        tuic = 23,
+        v2rayN = 99
+    }
+}
