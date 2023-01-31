9a0ec58557f1379d107d338be457c650e73a73cc
2dust <31833384+2dust@users.noreply.github.com>
2023-01-31 19:24:55 +0800

    up 6.7

---
+++ v2rayN/v2rayN/Converters/MaterialDesignFonts.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Windows.Media;
+
+namespace v2rayN.Converters
+{
+    public class MaterialDesignFonts
+    {
+        public static FontFamily MyFont { get; }
+
+        static MaterialDesignFonts()
+        {
+            var fontPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Resources\Fonts\");
+            MyFont = new FontFamily(new Uri($"file:///{fontPath}"), "./#Source Han Sans CN");
+        }
+    }
+}
