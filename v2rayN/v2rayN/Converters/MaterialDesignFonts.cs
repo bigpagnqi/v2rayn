a384a7d8de4195d123d1464b4107e6b004e60272
2dust <31833384+2dust@users.noreply.github.com>
2024-09-08 13:39:13 +0800

    up 6.58

---
+++ v2rayN/v2rayN/Converters/MaterialDesignFonts.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Media;
+
+namespace v2rayN.Converters
+{
+    public class MaterialDesignFonts
+    {
+        public static FontFamily MyFont { get; }
+
+        static MaterialDesignFonts()
+        {
+            try
+            {
+                var fontFamily = LazyConfig.Instance.Config.uiItem.currentFontFamily;
+                if (!Utils.IsNullOrEmpty(fontFamily))
+                {
+                    var fontPath = Utils.GetFontsPath();
+                    MyFont = new FontFamily(new Uri(@$"file:///{fontPath}\"), $"./#{fontFamily}");
+                }
+            }
+            catch
+            {
+            }
+            MyFont ??= new FontFamily("Microsoft YaHei");
+        }
+    }
+}
