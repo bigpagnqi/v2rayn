0151d2c33a38547ab81c9250536863902131840a
2dust <31833384+2dust@users.noreply.github.com>
2024-11-17 14:15:37 +0800

    up 7.1.2

---
+++ v2rayN/AmazTool/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace AmazTool
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine(LocalizationHelper.GetLocalizedValue("Guidelines"));
+                Thread.Sleep(5000);
+                return;
+            }
+
+            var fileName = Uri.UnescapeDataString(string.Join(" ", args));
+            UpgradeApp.Upgrade(fileName);
+        }
+    }
+}
