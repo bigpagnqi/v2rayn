043d3eff27007e4cdac2a6a521fd53212f55451f
2dust <31833384+2dust@users.noreply.github.com>
2026-06-07 11:23:36 +0800

    up 7.22.6

---
+++ v2rayN/v2rayN.Desktop/App.axaml.cs
@@ -0,0 +1,94 @@
+using v2rayN.Desktop.Common;
+using v2rayN.Desktop.Views;
+
+namespace v2rayN.Desktop;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            if (!Design.IsDesignMode)
+            {
+                AppManager.Instance.InitComponents();
+                DataContext = StatusBarViewModel.Instance;
+            }
+
+            desktop.Exit += OnExit;
+            desktop.MainWindow = new MainWindow();
+
+            if (OperatingSystem.IsMacOS())
+            {
+                Current?.TryGetFeature<IActivatableLifetime>()?.Activated += OnMacOSActivated;
+            }
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void OnMacOSActivated(object? sender, ActivatedEventArgs args)
+    {
+        if (args.Kind != ActivationKind.Reopen)
+        {
+            return;
+        }
+
+        Dispatcher.UIThread.Post(() =>
+        {
+            if (!AppManager.Instance.Config.UiItem.MacOSShowInDock)
+            {
+                MacAppUtils.SetActivationPolicyAccessory();
+            }
+            
+            ((ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow as MainWindow)?.ShowHideWindow(true);
+        });
+    }
+
+    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        if (e.ExceptionObject != null)
+        {
+            Logging.SaveLog("CurrentDomain_UnhandledException", (Exception)e.ExceptionObject);
+        }
+    }
+
+    private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+    {
+        Logging.SaveLog("TaskScheduler_UnobservedTaskException", e.Exception);
+    }
+
+    private void OnExit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
+    {
+    }
+
+    private async void MenuAddServerViaClipboardClick(object? sender, EventArgs e)
+    {
+        try
+        {
+            if (Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime { MainWindow: not null })
+            {
+                AppEvents.AddServerViaClipboardRequested.Publish();
+                await Task.Delay(1000);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logging.SaveLog("MenuAddServerViaClipboardClick", ex);
+        }
+    }
+
+    private async void MenuExit_Click(object? sender, EventArgs e)
+    {
+        await AppManager.Instance.AppExitAsync(false);
+        AppManager.Instance.Shutdown(true);
+    }
+}
